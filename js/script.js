35e5ab753646dc9430cd024123490bb6d0878777
sbrown7861 <skyler.brown15@yahoo.com>
2020-05-24 17:27:47 -0700

    Founders Membership
    Added a founders Membership Popup requested by Courtney

--- js/script.js
+++ js/script.js
@@ -5,46 +5,46 @@
 
 jQuery(function ($) {
 
-		// Function which adds the 'animated' class to any '.animatable' in view
-		var doAnimations = function () {
+	// Function which adds the 'animated' class to any '.animatable' in view
+	var doAnimations = function () {
 
-			// Calc current offset and get all animatables
-			var offset = $(window).scrollTop() + $(window).height(),
-				$animatables = $('.animatable');
+		// Calc current offset and get all animatables
+		var offset = $(window).scrollTop() + $(window).height(),
+			$animatables = $('.animatable');
 
-			// Check all animatables and animate them if necessary
-			$animatables.each(function (i) {
-				var $animatable = $(this);
+		// Check all animatables and animate them if necessary
+		$animatables.each(function (i) {
+			var $animatable = $(this);
 
-				// Items that are "above the fold"
-				if (($animatable.offset().top + $animatable.height() + 50) < offset) {
+			// Items that are "above the fold"
+			if (($animatable.offset().top + $animatable.height() + 50) < offset) {
 
-					// Item previously wasn't marked as "above the fold": mark it now
-					if (!$animatable.hasClass('animate-in')) {
-						$animatable.removeClass('animate-out').css('top', $animatable.css('top')).addClass('animate-in');
-					}
-
+				// Item previously wasn't marked as "above the fold": mark it now
+				if (!$animatable.hasClass('animate-in')) {
+					$animatable.removeClass('animate-out').css('top', $animatable.css('top')).addClass('animate-in');
 				}
 
-				// Items that are "below the fold"
-				else if (($animatable.offset().top + $animatable.height() + 50) > offset) {
+			}
 
-					// Item previously wasn't marked as "below the fold": mark it now
-					if ($animatable.hasClass('animate-in')) {
-						$animatable.removeClass('animate-in').css('top', $animatable.css('top')).addClass('animate-out');
-					}
+			// Items that are "below the fold"
+			else if (($animatable.offset().top + $animatable.height() + 50) > offset) {
 
+				// Item previously wasn't marked as "below the fold": mark it now
+				if ($animatable.hasClass('animate-in')) {
+					$animatable.removeClass('animate-in').css('top', $animatable.css('top')).addClass('animate-out');
 				}
 
-			});
+			}
 
-		};
+		});
 
-		// Hook doAnimations on scroll, and trigger a scroll
-		$(window).on('scroll', doAnimations);
-		$(window).trigger('scroll');
+	};
 
-	});
+	// Hook doAnimations on scroll, and trigger a scroll
+	$(window).on('scroll', doAnimations);
+	$(window).trigger('scroll');
+
+});
 
 
 $("document").ready(function () {
@@ -73,3 +73,24 @@ $("document").ready(function () {
 	});
 
 });
+
+
+/*Founders Membership */
+
+window.onload = function () {
+	$("#overlay-founders")
+		.fadeIn()
+		.find("#modal-founders")
+		.fadeIn();
+
+
+	$(".close").on("click", function (event) {
+		event.preventDefault();
+		$("#overlay-founders")
+			.fadeOut()
+			.find("#modal-founders")
+			.fadeOut();
+	});
+};
+
+/*End Founders Membership*/
